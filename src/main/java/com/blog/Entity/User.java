e28df79d55e45dc407f5b0c73eb0f3899ce09013
Prativa-Biswas <prativa.biswas000@gmail.com>
2026-04-10 14:48:40 +0530

    feat: added Initial Project Structure

---
+++ src/main/java/com/blog/Entity/User.java
@@ -0,0 +1,21 @@
+package com.blog.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+
+}
